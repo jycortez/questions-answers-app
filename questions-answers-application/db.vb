f23c97e380a5b5ff4e6ac4064c222425c27d833d
jycortez <jycortez@email.uark.edu>
2017-10-26 09:14:33 -0500

    add execute method

--- questions-answers-application/db.vb
+++ questions-answers-application/db.vb
@@ -40,4 +40,19 @@ Public Class db
             End If
         End Try
     End Sub
+
+
+    Public Sub execute(ByRef dgv As DataGridView)
+        Try
+            connection.Open()
+            command.ExecuteNonQuery()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Throw ex
+        Finally
+            If connection.State = ConnectionState.Open Then
+                connection.Close()
+            End If
+        End Try
+    End Sub
 End Class
